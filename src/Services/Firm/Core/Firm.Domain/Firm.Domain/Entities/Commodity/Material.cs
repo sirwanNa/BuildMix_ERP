6da5f4ea98a279576633583d46108d507e7576c1
Asus <Asus@DESKTOP-B69ULGL>
2025-09-21 22:23:18 -0700

    #RevisionHamid2

--- src/Services/Firm/Core/Firm.Domain/Firm.Domain/Entities/Commodity/Material.cs
+++ src/Services/Firm/Core/Firm.Domain/Firm.Domain/Entities/Commodity/Material.cs
@@ -1,11 +1,29 @@
 ﻿namespace Firm.Domain.Entities.Commodity
 {
+    /// <summary>
+    /// مواد اولیه تشکیل دهنده محصولات
+    /// </summary>
     public class Material:BaseEntity
     {
+        /// <summary>
+        /// کد محصول اصلی
+        /// </summary>
         public Guid MainProductId { get; set; }
+        /// <summary>
+        /// محصول اصلی
+        /// </summary>
         public virtual Product? MainProduct { get; set; }
+        /// <summary>
+        /// کد محصول فرعی
+        /// </summary>
         public Guid SubProductId { get; set; }
+        /// <summary>
+        /// محصول فرعی
+        /// </summary>
         public virtual Product? SubProduct { get; set; }
+        /// <summary>
+        /// مقدار مصرف
+        /// </summary>
         public float Amount { get; set; }
     }
 }
--- src/Services/Firm/Core/Firm.Domain/Firm.Domain/Entities/Commodity/Product.cs
+++ src/Services/Firm/Core/Firm.Domain/Firm.Domain/Entities/Commodity/Product.cs
@@ -2,10 +2,22 @@
 
 namespace Firm.Domain.Entities.Commodity
 {
+    /// <summary>
+    /// محصولات
+    /// </summary>
     public class Product:BaseEntity
     {
+        /// <summary>
+        /// عنوان محصول
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// کد واحد اندازه گیری
+        /// </summary>
         public Guid UnitId { get; set; }
+        /// <summary>
+        /// واحد اندازه گیری
+        /// </summary>
         public virtual Unit? Unit { get; set; }
     }
 }
